4315ece96bbb0a21ab08fbf17fc5aafb944ceb6a
Noemi Dvořáková <noemika.nd@gmail.com>
2024-10-07 17:22:30 +0200

    Přidejte soubory projektu.

---
+++ Dvorakova_BCSH2_SemestralniPrace/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Dvorakova_BCSH2_SemestralniPrace
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
